62a817188f9d05e108127e6d709a4f0da86dd88d
liani250505 <norliani083@gmail.com>
2024-10-05 00:30:39 -0700

    sudah pak

---
+++ bin/Function_Parameter.dart
@@ -0,0 +1,9 @@
+//Function Parameter
+void sayHello(String firstName, String lastName) {
+  print('Hello $firstName $lastName');
+}
+
+void main(List<String> args) {
+  sayHello('Liani', 'Selly');
+  sayHello('Nelly', 'Liani');
+}
